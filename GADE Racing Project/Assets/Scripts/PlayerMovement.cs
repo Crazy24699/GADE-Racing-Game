3d6a71c571f0dedd92890d0497de61d70df93db6
Crazy24699 <carlvschulze2@gmail.com>
2023-08-14 08:53:46 +0200

    Compiled Shaders

--- GADE Racing Project/Assets/Scripts/PlayerMovement.cs
+++ GADE Racing Project/Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
     public GameObject PlayerRef;
     
     public float MoveSpeed = 50;
+
+
+    public Rigidbody Rigidbody;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +20,21 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MoveMethodPH();
     }
 
 
 
     public void MoveMethodPH()
     {
+        float HorizontalSpeed = Input.GetAxis("Horizontal");
+        float VerticalSpeed = Input.GetAxis("Vertical");
+
+        Vector3 MoveDirection = new Vector3(HorizontalSpeed * MoveSpeed, Rigidbody.velocity.y, VerticalSpeed * MoveSpeed);
+
+        Rigidbody.velocity = MoveDirection;
         
+
     }
 }
 
