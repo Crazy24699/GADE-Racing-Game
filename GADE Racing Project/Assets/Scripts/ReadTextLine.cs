291a497db47b2582752dae7448ec675888d41a2b
Crazy24699 <110395759+Crazy24699@users.noreply.github.com>
2023-10-21 10:57:15 +0200

    Pulled the map

--- GADE Racing Project/Assets/Scripts/ReadTextLine.cs
+++ GADE Racing Project/Assets/Scripts/ReadTextLine.cs
@@ -10,14 +10,5 @@ public class ReadTextLine : MonoBehaviour
     private string filePath;
     private int currentLineIndex = 0;
 
-    private void Start()
-    {
-        filePath = Application.dataPath + "/Dialog.txt";
-        myText.text = GetLineAtIndex(currentLineIndex);
-    }
 
-    private string GetLineAtIndex(int index)
-    {
-        string
-    }
 }
